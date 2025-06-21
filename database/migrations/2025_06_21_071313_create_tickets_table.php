d93b9b84c5f594e8ccefff30ce3115f96639f0bb
shiva_jella <shivajella6300@gmail.com>
2025-06-21 12:48:38 +0530

    Ticket table creation

--- database/migrations/2025_06_21_071313_create_tickets_table.php
+++ database/migrations/2025_06_21_071313_create_tickets_table.php
@@ -11,14 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tickets', function (Blueprint $table) {
+        
+          Schema::create('tickets', function (Blueprint $table) {
             $table->id('ticket_id');
             $table->string('User_Name');
-            $table->string('Ticket_name');
+            $table->string('Ticket_Subject');
+            $table->string('Ticket_Category');
             $table->string('Ticket_Description');
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
